415f0c8b1fd6d41cf99fd53c08381184a2b8a3fc
Onur YILDIZ <onuryldz008@gmail.com>
2022-09-07 17:16:12 +0300

    .

--- src/Common.Architecture.Shared/TransferObjects/Idendity/UserForLoginDto.cs
+++ src/Common.Architecture.Shared/TransferObjects/Idendity/UserForLoginDto.cs
@@ -9,28 +9,7 @@ namespace Common.Architecture.Shared.TransferObjects.Idendity
 {
     public class UserForLoginDto : IDto
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Title { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Token { get; set; }
-        public int ExpirationMinutes { get; set; }
-
-        public string UserRolesCsv { get; set; }
-        public Guid? CurrentRoleId { get; set; }
-        public string CurrentRoleName { get; set; }
-        public string LdapKod { get; set; }
-
-        public UserRoleDto[] UserRoles { get; set; }
-        public long? PersonelTanimId { get; set; }
-        public long? PersonelSicilNo { get; set; }
-        public string SorumluBirimList { get; set; }
-
-        // This properties are required when login the system. For this function -->  _signInManager.CheckPasswordSignInAsync()
-        public string PasswordHash { get; set; }
-        public bool EmailConfirmed { get; set; }
     }
 }
